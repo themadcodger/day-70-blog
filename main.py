77d650ffb8b638151ddf908995a4b5a6a46a5963
ollie <green.base8525@fastmail.com>
2026-09-07 17:26:44 -0700

    Clean up main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-# Optional: add contact me email functionality (Day 60)
 # import smtplib
 import os
 
